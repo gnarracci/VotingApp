a2d61e2cd23518fef88ae6283263dd2d2b0c628e
Gianni Narracci <gnarracci@gmail.com>
2020-10-08 02:01:08 -0400

    Initial Server Config

---
+++ server/src/app.mjs
@@ -0,0 +1,13 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+// Settings
+app.set("port", process.env.PORT || 4000);
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+export default app;
---
+++ server/src/database.mjs
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost/votingApp_db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then((db) => console.log("DB is Connected!"))
+  .catch((error) => console.log(error));
